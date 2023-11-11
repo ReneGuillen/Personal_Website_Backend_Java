1f2a59fe9c5e74441d4b54135eee1499ebd5779e
Josue <renguill47@gmail.com>
2023-11-11 13:07:54 -0600

    Add SDK and DynamoDB dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.apache.httpcomponents.client5:httpclient5:5.0")
+    implementation(platform("software.amazon.awssdk:bom:2.21.1")) // AWS SDK v2 BOM
+    implementation("software.amazon.awssdk:dynamodb")
 }
 
 tasks.test {
